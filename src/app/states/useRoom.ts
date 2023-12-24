e8b06a3e3d9300b7be8c32b1f351b4cf1f2a537c
Aristidescosta <aristidiscosta200@gmail.com>
2023-12-24 10:12:25 +0100

    refactor: criado o hook useRoom

--- src/app/states/useRoom.ts
+++ src/app/states/useRoom.ts
@@ -1,22 +1,46 @@
-import { create } from 'zustand'
-import { IUserType } from '../types/UserType'
-import { devtools, persist } from 'zustand/middleware'
+import { useEffect, useState } from "react";
+import { getAllQuestions } from "../repository/RoomRepository";
+import { ROOM_REF } from "../utils/constants";
+import { IQuestionType } from "../types/QuestionType";
 
-interface IUserStates {
-  user: IUserType | undefined
-  setUser: (user: IUserType) => void
+type IFirebaseQuestions = Record<string, IQuestionType>;
+
+type TQuestionProps = IQuestionType & {
+  id: string;
+};
+
+export const useRoom = (roomId: string | undefined) =>{
+  const [titleRoom, setTitleRoom] = useState("");
+  const [questions, setQuestions] = useState<TQuestionProps[]>([]);
+
+  useEffect(() => {
+    if (roomId) {
+      getAllQuestions(ROOM_REF, roomId).then((response) => {
+        if (typeof response === "string") console.log(response);
+        else {
+          const databaseRoom = response.val();
+
+          const firebaseQuestions: IFirebaseQuestions =
+            databaseRoom.questions ?? {};
+
+          const parseQuestions = Object.entries(firebaseQuestions).map(
+            ([key, question]) => {
+              return {
+                id: key,
+                author: question.author,
+                content: question.content,
+                isAnswered: question.isAnswered,
+                isHighLigted: question.isHighLigted,
+              };
+            }
+          );
+
+          setQuestions(parseQuestions);
+          setTitleRoom(databaseRoom.title);
+        }
+      });
+    }
+  }, [roomId, questions]);
+
+  return { questions, titleRoom }
 }
-
-export const useAuth = create<IUserStates>()(
-  devtools(
-    persist(
-      (set) => ({
-        user: undefined,
-        setUser: (user: IUserType) => set(() => ({ user: user }))
-      }),
-      {
-        name: "user-storage"
-      }
-    )
-  )
-)
--- src/app/ui/pages/Room/index.tsx
+++ src/app/ui/pages/Room/index.tsx
@@ -14,35 +14,30 @@ import { IQuestionType } from "../../../types/QuestionType";
 import { LetButton } from "../../components/LetButton";
 import logoImg from "../../../../../public/logo.svg";
 import { RoomCode } from "../../components/RoomCode";
-import { ROOM_REF } from "../../../utils/constants";
 import { useAuth } from "../../../states/useAuth";
 import { Link, useParams } from "react-router-dom";
 import {
-  createQuestion,
-  getAllQuestions,
+  createQuestion
 } from "../../../repository/RoomRepository";
 import { Question } from "../../components/Question";
+import { useRoom } from "../../../states/useRoom";
 
 type IRoomParams = {
   id: string;
 };
 
-type IFirebaseQuestions = Record<string, IQuestionType>;
 
-type TQuestionProps = IQuestionType & {
-  id: string;
-};
 
 export const Room: React.FC = () => {
   const { id: roomId } = useParams<IRoomParams>();
 
-  const [newQuestion, setNewQuestion] = useState("");
+  
   const { toastMessage, ToastStatus } = useToastMessage();
   const { user } = useAuth();
+  const { questions, titleRoom } = useRoom(roomId)
+ 
+  const [newQuestion, setNewQuestion] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [titleRoom, setTitleRoom] = useState("");
-
-  const [questions, setQuestions] = useState<TQuestionProps[]>([]);
 
   const copyRooCodeToClipboard = useCallback(() => {
     if (roomId) {
@@ -107,34 +102,7 @@ export const Room: React.FC = () => {
     }
   }, [newQuestion, user, roomId]);
 
-  useEffect(() => {
-    if (roomId) {
-      getAllQuestions(ROOM_REF, roomId).then((response) => {
-        if (typeof response === "string") console.log(response);
-        else {
-          const databaseRoom = response.val();
-
-          const firebaseQuestions: IFirebaseQuestions =
-            databaseRoom.questions ?? {};
-
-          const parseQuestions = Object.entries(firebaseQuestions).map(
-            ([key, question]) => {
-              return {
-                id: key,
-                author: question.author,
-                content: question.content,
-                isAnswered: question.isAnswered,
-                isHighLigted: question.isHighLigted,
-              };
-            }
-          );
-
-          setQuestions(parseQuestions);
-          setTitleRoom(databaseRoom.title);
-        }
-      });
-    }
-  }, [roomId, questions]);
+  
 
   return (
     <Box>
